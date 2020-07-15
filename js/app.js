7db5bca4099c481f5389f577cdd5c2f28f5bcf92
MElborolossy <mohammedelborolossy@gmail.com>
2020-07-15 07:25:11 +0200

    Add New Features Hide nav bar & back to top button

--- js/app.js
+++ js/app.js
@@ -20,20 +20,35 @@
 const bodyElement = document.querySelector('body');
 const allSections = document.getElementsByTagName('section');
 const navBarList = document.getElementById('navbar__list');
-let timer;
-let activeSection = 'section1';
-let isScrolling;
+const headerElement = document.querySelector('.page__header');
+const topButton = document.querySelector('#backTop');
+let isScrollingStopped;
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-let scrollToSection = function(ev){
-    event.preventDefault();
-    const section = document.querySelector(`#${ev.target.innerHTML}`);
-    section.scrollIntoView({behavior : 'smooth'});
+
+/* hideHeader helper function to hide the navigation bar */
+let hideHeader = function () {
+    headerElement.style.opacity = '0';
 }
+
+/* showBackTopButton helper function to detect if page not on the top */
+let showBackTopButton = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+/* goToTop is the onclick function for the backTop button */
+let goToTop = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 /**
  * End Helper Functions
@@ -41,7 +56,7 @@ let scrollToSection = function(ev){
  * 
 */
 
-// build the nav
+/* build the nav */
 let buildNavigationMenu = function () {
     const virtualDom = document.createDocumentFragment();
     for (const section of allSections) {
@@ -53,35 +68,38 @@ let buildNavigationMenu = function () {
     navBarList.appendChild(virtualDom);
 }
 
-
-
-// Add class 'active' to section when near top of viewport
+/* Add class 'active' to section when near top of viewport */
 let inViewPort = function () {
     let nearTop = [];
     let rect, i;
     i = 0;
-    
-    // Calaculte top boundry value for each section 
+
+    /* Calaculte top boundry value for each section */ 
     for (const section of allSections) {
         rect = section.getBoundingClientRect();
         nearTop[i] = Math.abs(rect.top);
         i += 1;
     }
-   
-    // Delete Class Attribute from the old section
-    if(document.querySelector('section.active') != null && document.querySelector('a.active') != null){
+
+    /* Delete Class Attribute from the old section */
+    if (document.querySelector('section.active') != null && document.querySelector('a.active') != null) {
         document.querySelector('section.active').removeAttribute('class');
         document.querySelector('a.active').removeAttribute('class');
     }
 
-    // Add Class Attribute to the class near to the top
+    /* Add Class Attribute to the class near to the top */
     let inViewSectionNum = nearTop.indexOf(Math.min(...nearTop));
     let inViewSectionElement = document.querySelectorAll('section')[inViewSectionNum];
     inViewSectionElement.setAttribute('class', 'active');
     document.querySelector(`a[href='#${inViewSectionElement.getAttribute('id')}']`).setAttribute('class', 'active');
 }
-// Scroll to anchor ID using scrollTO event
 
+/* Scroll to anchor ID using scrollTO event */
+let scrollToSection = function (ev) {
+    event.preventDefault();
+    const section = document.querySelector(`#${ev.target.innerHTML}`);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
 
 /**
  * End Main Functions
@@ -89,11 +107,26 @@ let inViewPort = function () {
  *
 */
 
-// Build menu 
+/* Build menu */
 document.addEventListener('DOMContentLoaded', buildNavigationMenu);
 
-// Scroll to section on link click
-navBarList.addEventListener('click',scrollToSection);
+/* Scroll to section on link click */
+navBarList.addEventListener('click', scrollToSection);
 
-// Set sections as active
-document.addEventListener('scroll', inViewPort);
+/* Set sections as active and hide the navigation bar if not scrolling */
+document.addEventListener('scroll', function () {
+    clearTimeout(isScrollingStopped);
+    inViewPort();
+    showBackTopButton();
+    headerElement.style.opacity = '1';
+    isScrollingStopped = setTimeout(hideHeader, 100);
+});
+
+/* Show navigation bar if mouseover it */
+headerElement.addEventListener('mouseover', function () {
+    clearTimeout(isScrollingStopped);
+    headerElement.style.opacity = '1';
+});
+
+/* hide navigation bar after mouse move out from it */
+headerElement.addEventListener('mouseout', hideHeader);
